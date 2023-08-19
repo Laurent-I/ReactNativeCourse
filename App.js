d16d9d841b55bf46ceda782a7ae2ccda3ff1822c
Laurent-I <irakramlaw@gmail.com>
2023-08-19 18:39:27 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
 
 export default function App() {
+  const {ColorScheme, toggleColorScheme} = useColorScheme('light')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} className='dark:bg-slate-700 bg-slate-300'>
+      <Switch value={ColorScheme === 'dark'} onChange={toggleColorScheme}/>
+      <Text style = {styles.dummyText} className='dark:text-white text-xl'> Hello World!!</Text>
+      <Text style={styles.dummyText} className='dark:text-white'>Laurent is so amazing</Text>
+      <Button title='Cool'/>
     </View>
   );
 }
@@ -17,4 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText:{
+    margin: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding:16
+  }
 });
